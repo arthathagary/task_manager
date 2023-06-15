9291e1ba465cefe9cbd3e3e262b8f67cacd0f453
Arthath Agary <arthathagary@gmail.com>
2023-06-15 23:57:43 +0530

    All Controllers Done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,24 @@
 const Task = require('../models/Task')
-const getAllTasks = (req,res)=>{
-   res.send("Get All Tasks")
+
+const getAllTasks = async (req,res)=>{
+   try {
+      const tasks = await Task.find({})
+      res.status(200).json({tasks});
+   } catch (error) {
+      res.status(404).json({error})
+   }
+   
 }
 
-const getSingleTask =(req,res)=>{
-   const {id:taskID} = req.params;
-   res.send(`getSingleTask ${taskID}`)
+const getSingleTask = async (req,res)=>{
+   try {
+      const {id:taskID} = req.params;
+      const task = await Task.findOne({_id:taskID})
+      res.status(200).json({task});
+   } catch (error) {
+      res.status(404).json({error})
+   }
+   
 }
 
 const createTask = async (req,res)=>{
@@ -13,17 +26,31 @@ const createTask = async (req,res)=>{
       const task = await Task.create(req.body)
       res.status(200).json({task})
    } catch (error) {
-      console.log(error);
+      res.status(404).json({error});
    }
    
 }
 
-const deleteTask = (req,res)=>{
-   res.send('deleteTask')
+const deleteTask = async (req,res)=>{
+   try {
+      const {id:taskID} = req.params;
+      const task = await Task.findOneAndDelete({_id:taskID})
+      res.status(200).json({task})
+   } catch (error) {
+      res.status(404).json({error});
+   }
+   
 }
 
-const updateTask = (req,res)=>{
-   res.send('updateTask')
+const updateTask = async (req,res)=>{
+   try {
+      const {id:taskID} = req.params;
+      const task = await Task.findOneAndUpdate({_id:taskID},req.body,{new:true});
+      res.status(200).json({task})
+   } catch (error) {
+      res.status(404).json({error})
+   }
+   
 }
 
 module.exports={
